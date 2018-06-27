3713921edc2ea62140c5fcd69679adc9061c4df4
bjsmith3185 <bjsmith3185@hotmail.com>
2018-06-27 13:35:16 -0400

    restart() reset() updateArray()

--- assets/main.js
+++ assets/main.js
@@ -3,51 +3,38 @@ $(document).ready(function() {
     $(".container-game-screen").hide();
 
 
-var letters =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
+var letters =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","_"," "];
 var main = $("body");
-
 var btns = main.find("#letter-area");
-
 var selectedLetter;
-
-
-
-
-
-
-var movies = ["top gun", "dinasour park", "gun fighter"];
-
+var movies = ["top gun", "dinasour park", "gun fighter","fast and furious","notebook","forrest gump","frozen"];
 var songs = ["wildflowers", "down south", "refugee"];
-
 var burgers = ["tomato", "mustard", "bacon"];
-
 var cars = ["ford", "chevy", "dodge", "lexus"];
-
 var selectedWord;
-
 var category;
-
 var wordToGuessArray = [];
-
 var lengthOfWordToGuess;
-
 var displayArray = [];
-
-
-var populateArray = false;
-
+var nextWord = false;
+var wins = 0;
+var losses = 0;
+var misses = 0;
 
 
 
 function selectWord(category) {
     if (category === "movies") {
+        $("#category-name").text("Movie titles");
         selectedWord = movies[Math.floor(Math.random() * movies.length)];
         } else if (category === "songs") {
+            $("#category-name").text("Song titles");
         selectedWord = songs[Math.floor(Math.random() * songs.length)];
     } else if (category === "burgers") {
+        $("#category-name").text("Burger toppings");
         selectedWord = burgers[Math.floor(Math.random() * burgers.length)];
     } else if (category === "cars") {
+        $("#category-name").text("Car manufacturers");
         selectedWord = cars[Math.floor(Math.random() * cars.length)];
     }
     console.log("selected word for array: " + selectedWord);
@@ -55,7 +42,7 @@ function selectWord(category) {
     // insert selected word into array
     
     lengthOfWordToGuess = selectedWord.length;
-    console.log("length of word: " + lengthOfWordToGuess);
+    // console.log("length of word: " + lengthOfWordToGuess);
 
     wordToGuessArray = selectedWord.split("");
     console.log("guess array: " + wordToGuessArray);
@@ -64,26 +51,27 @@ function selectWord(category) {
         displayArray.push("_");
         
     }
-    console.log("displayArray: " + displayArray);
-    // $("#guess-area").text(displayArray.join(" "));
+    // console.log("displayArray: " + displayArray);
+    $("#guess-area").text(displayArray.join(" "));
 
     
 };
 
 function createButtons() {
+    $("#letter-area").empty();
     for (var i = 0; i < letters.length; i++) {
         var letterButton = $("<button>");
-        letterButton.addClass("letter letter-btn-color");  //letter-button  letter-button-color
-        letterButton.attr("data-letter", letters[i]);
-        letterButton.text(letters[i]);
+        letterButton.addClass("letter letter-btn-color").attr("data-newletter", letters[i]).text(letters[i]);  //letter-button  letter-button-color
+        // letterButton.attr("data-letter", letters[i]);
+        // letterButton.text(letters[i]);
         btns.append(letterButton);
       }
 };
 
 function pickLetter() {
     btns.on("click", ".letter", function() {
-        selectedLetter = $(this).attr("data-letter");
-        console.log("this is the letter selected: " + selectedLetter);
+        selectedLetter = $(this).attr("data-newletter");
+        // console.log("this is the letter selected: " + selectedLetter);
         $(this).hide();
         checkLetter(selectedLetter);
       });
@@ -93,11 +81,12 @@ function pickLetter() {
 function checkLetter(selectedLetter) {
     for (var i = 0; i < lengthOfWordToGuess; i++) {
         if(selectedLetter === wordToGuessArray[i]) {
-            populateArray = true;
-            console.log("is in the word: " + populateArray);
+           
+            // console.log("is in the word: " + populateArray);
             updateArray(selectedLetter);
         } else {
-            populateArray = false;
+          
+            misses++;
             // send a signal to reduce number of guesses left
         }
     }
@@ -108,25 +97,67 @@ function updateArray (selectedLetter) {
     for (var i = 0; i < lengthOfWordToGuess; i++) {
         if(selectedLetter === wordToGuessArray[i]) {
             displayArray[i] = selectedLetter;
-            console.log(displayArray);
-            console.log(wordToGuessArray);
+            // console.log(displayArray);
+            // console.log(wordToGuessArray);
+            $("#guess-area").text(displayArray.join(" "));
             if (displayArray.toString() === wordToGuessArray.toString()) {
-                    alert("you won");
+                    // alert("you won");
+                    wins++;
+                    nextWord = true;
+                    console.log("this is nextWord status: " + nextWord);
+                    console.log("this is category: " + category);
+                    reset();
+
             }
         }
     };
 };
 
 
+function reset() {
+    if (nextWord) {
+        nextWord = false;
+        createButtons();
+        selectWord(category);
+        pickLetter();
+        $("#guess-area").empty();
+        $(".wins").text(wins);
+        $(".losses").text(losses);
+    }
+}
+
+
 $("body").on("click", ".category-buttons", function() {
     category = $(this).val();
-    console.log("category: " + category);
+    // console.log("category: " + category);
     $(".container-welcome-screen").hide();
     $(".container-game-screen").show();
     selectWord(category);
     createButtons();
     pickLetter();
+    $(".wins").text(wins);
+    $(".losses").text(losses);
   
+});
+
+
+function restart() {
+    btns = main.find("#letter-area");
+    selectedLetter;
+    selectedWord;
+    category;
+    wordToGuessArray = [];
+    lengthOfWordToGuess;
+    displayArray = [];
+    wins = 0;
+    losses = 0;
+    $(".container-game-screen").hide();
+    $(".container-welcome-screen").show();
+    $("#letter-area").empty();
+};
+
+$("#restart").on("click", function() {
+    restart();
 });
 
 });
