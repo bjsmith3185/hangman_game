667ed9ae79798ce980899ee03a66bad458e37ac3
bjsmith3185 <bjsmith3185@hotmail.com>
2018-07-01 14:56:30 -0400

    badges added to index.html

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,251 +1,221 @@
 $(document).ready(function() {
+    
 
+    var letters =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","_"];
+    var main = $("body");
+    var buttons = main.find("#letter-area");
+    var selectedLetter;
+    var arcade = ["pacman", "space_invaders", "pong", "donkey_kong", "aseroids", "centipede", "dig_dug", "pole_position", "spy_hunter", "commando"];
+    var movies = ["terminator", "commando", "predator","twins","true_lies","kindergarten_cop","pumping_iron"];
+    var songs = ["keep_on_loving_you", "celebration", "who_can_it_be_now", "mickey", "beat_it", "all_night_long", "time_after_time", "the_power_of_love", "we_build_this_city", "livin_on_a_prayer", "la_bamba", "welcome_to_the_jungle", "free_fallin"];
+    var burgers = ["tomato", "mustard", "bacon", "ketchup", "mayo", "lettuce", "egg", "pickles"];
+    var cars = ["ford", "chevy", "dodge", "lexus", "kia", "toyota", "honda", "jeep", "chrystler"];
+    var wwe = ["big_show", "brock_lesnar","matt_hardy", "the_rock", "steve_austin", "undertaker", "cm_punk", "john_cena", "booker_t", "hhh", "golddust", "aj_styles", "carmella", "dolph_ziggler", "billy_gunn", "big_bossman" ];
+    var count = 1;
+    var selectedWord;
+    var category;
+    var wordToGuessArray = [];  
+    var lengthOfWordToGuess; 
+    var displayArray = []; 
+    var isInArray = false;
+    var wins = 0;
+    var losses = 0;
+    var misses = 0;
+    var gameOn = true;
+    
+    $(".container-welcome-screen").show();
     $(".container-game-screen").hide();
     $(".container-loss-screen").hide();
-
-
-var letters =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","_"," "];
-var main = $("body");
-var btns = main.find("#letter-area");
-var selectedLetter;
-var movies = ["top gun", "dinasour park", "gun fighter","fast and furious","notebook","forrest gump","frozen"];
-var songs = ["wildflowers", "down south", "refugee"];
-var burgers = ["tomato", "mustard", "bacon"];
-var cars = ["ford", "chevy", "dodge", "lexus"];
-var selectedWord;
-var category;
-var wordToGuessArray = [];
-var lengthOfWordToGuess;
-var displayArray = [];
-var isInArray = false;
-var nextWord = false;
-var wins = 0;
-var losses = 0;
-var misses = 0;
-var gameOn = true;
-
-
-
-function selectWord(category) {
-    displayArray = [];
-    if (category === "movies") {
-        $("#category-name").text("Movie titles");
-        selectedWord = movies[Math.floor(Math.random() * movies.length)];
-        } else if (category === "songs") {
-            $("#category-name").text("Song titles");
-        selectedWord = songs[Math.floor(Math.random() * songs.length)];
-    } else if (category === "burgers") {
-        $("#category-name").text("Burger toppings");
-        selectedWord = burgers[Math.floor(Math.random() * burgers.length)];
-    } else if (category === "cars") {
-        $("#category-name").text("Car manufacturers");
-        selectedWord = cars[Math.floor(Math.random() * cars.length)];
-    }
-    console.log("selected word for array: " + selectedWord);
-
-    // insert selected word into array
+    $(".container-win-screen").hide();
     
-    lengthOfWordToGuess = selectedWord.length;
-    // console.log("length of word: " + lengthOfWordToGuess);
-
-    wordToGuessArray = selectedWord.split("");
-    console.log("guess array: " + wordToGuessArray);
-
-    for (var i = 0; i < lengthOfWordToGuess; i++) {
-        displayArray.push("_");
-        
-    }
-    // console.log("displayArray: " + displayArray);
-    $("#guess-area").text(displayArray.join(" "));
-
     
-};
-
-function createButtons() {
-    $("#letter-area").empty();
-    for (var i = 0; i < letters.length; i++) {
-        var letterButton = $("<button>");
-        letterButton.addClass("letter letter-btn-color").attr("data-newletter", letters[i]).text(letters[i]);  //letter-button  letter-button-color
-        // letterButton.attr("data-letter", letters[i]);
-        // letterButton.text(letters[i]);
-        btns.append(letterButton);
-      }
-};
-
-function pickLetter() {
-    if (gameOn) {
-    btns.on("click", ".letter", function() {
-        selectedLetter = $(this).attr("data-newletter");
-        console.log("this is the letter selected: " + selectedLetter);
-        $(this).hide();
-        checkLetter(selectedLetter);
-        console.log("after letter picked, misses: " + misses);
-      });
-    }
-};
-
-
-function checkLetter(selectedLetter) {
-    isInArray = false;
-    console.log("isinarray should be false: " + isInArray);
-    for (var i = 0; i < lengthOfWordToGuess; i++) {
-        if(selectedLetter === wordToGuessArray[i]) {
-            isInArray = true;
-            console.log("isinarray should be true: " + isInArray);
-            // game(selectedLetter);
-            // console.log("is in the word: " + populateArray);
-            // updateArray(selectedLetter);
-        // } else {
-        //     isInArray = false;
-        //     game(selectedLetter);
-        //     // misses++;
-
-        //     // if(misses === 6){
-        //     //     $(".container-loss-screen").show();
-        //     // }
-
-        //     // send a signal to reduce number of guesses left
-        // }
-    } 
-    } 
-        if (isInArray) {
-            game(selectedLetter);
-        } else {
-            misses++;
-            console.log("this is misses " + misses);
-            didIWin();
-        }
-    
-};
-
-
-function game(selectedLetter) {
-    if (isInArray) {
-        for (var i = 0; i < lengthOfWordToGuess; i++) {
-            if(selectedLetter === wordToGuessArray[i]) {
-                displayArray[i] = selectedLetter;
-                // console.log(displayArray);
-                // console.log(wordToGuessArray);
-                $("#guess-area").text(displayArray.join(" "));
-                didIWin();
-            } 
-        }
-    } 
-    // else {
-    //     misses++;
-    //     console.log("this is misses " + misses);
-    //     didIWin();
-    // }
-};
-
-function didIWin(){
-    if (displayArray.toString() === wordToGuessArray.toString()) {
-            // alert("you won");
-            wins++;
-            nextWord = true;
-            // console.log("this is nextWord status: " + nextWord);
-            // console.log("this is category: " + category);
-            reset();
-
-    }
-    if (misses === 6) {
-        gameOn = false;
-        losses++;
-        $(".container-loss-screen").show();
-    }
-};
-
-$("#nextWord").on("click", function() {
-    nextWord = true;
-    reset();
-    $(".container-loss-screen").hide();
-});
-
-
-
-// function updateArray (selectedLetter) {
-//     for (var i = 0; i < lengthOfWordToGuess; i++) {
-//         if(selectedLetter === wordToGuessArray[i]) {
-//             displayArray[i] = selectedLetter;
-//             // console.log(displayArray);
-//             // console.log(wordToGuessArray);
-//             $("#guess-area").text(displayArray.join(" "));
-//             if (displayArray.toString() === wordToGuessArray.toString()) {
-//                     // alert("you won");
-//                     wins++;
-//                     nextWord = true;
-//                     console.log("this is nextWord status: " + nextWord);
-//                     console.log("this is category: " + category);
-//                     reset();
-
-//             }
-//         }
-//     };
-// };
-
-
-function reset() {
-    if (nextWord) {
-        nextWord = false;
-        gameOn = true;
-        displayArray = [];
-        $("#guess-area").empty();
+    $("body").on("click", ".category-buttons", function() {
+        category = $(this).val();
+        $(".container-game-screen").show();
+        $(".container-welcome-screen").hide();
+        $(".container-loss-screen").hide();
+        $(".container-win-screen").hide();
         $(".wins").text(wins);
         $(".losses").text(losses);
-        selectWord(category);
-        createButtons();
-        pickLetter();
-        
-    }
-}
-
-
-$("body").on("click", ".category-buttons", function() {
-    category = $(this).val();
-    // console.log("category: " + category);
-    $(".container-welcome-screen").hide();
-    $(".container-game-screen").show();
-    selectWord(category);
-    createButtons();
-    pickLetter();
-    $(".wins").text(wins);
-    $(".losses").text(losses);
-  
-});
-
-function startGame() {
-    selectWord(category);
-    createButtons();
-    pickLetter();
-    misses = 0;
+       
+        startGame(category);
+    });
     
-
-}
-
-
-function restart() {
-    btns = main.find("#letter-area");
-    selectedLetter;
-    selectedWord;
-    category;
-    wordToGuessArray = [];
-    lengthOfWordToGuess;
-    displayArray = [];
-    wins = 0;
-    losses = 0;
-    $(".container-game-screen").hide();
-    $(".container-welcome-screen").show();
-    $("#letter-area").empty();
-    $(".container-loss-screen").hide();
-};
-
-$("#restart").on("click", function() {
-    restart();
-});
-
-});
-
-
-// need a true/false to allow user to click letter buttons.
-// create an update score function to run after the letter button onlcick event. 
-// create a function to update a picture of a hang man. 
+    
+    function startGame(category) {
+        misses = 0;
+        displayArray = [];
+        lengthOfWordToGuess;
+        wordToGuessArray = [];
+        selectedWord;
+        if (category === "movies") {
+            $("#category-name").text("Movie titles");
+            selectedWord = movies[Math.floor(Math.random() * movies.length)];
+            } else if (category === "songs") {
+                $("#category-name").text("Song titles");
+            selectedWord = songs[Math.floor(Math.random() * songs.length)];
+        } else if (category === "burgers") {
+            $("#category-name").text("Burger toppings");
+            selectedWord = burgers[Math.floor(Math.random() * burgers.length)];
+        } else if (category === "cars") {
+            $("#category-name").text("Car manufacturers");
+            selectedWord = cars[Math.floor(Math.random() * cars.length)];
+        } else if (category === "wwe") {
+            $("#category-name").text("WWE Wrestlers");
+            selectedWord = cars[Math.floor(Math.random() * cars.length)];
+        } else if (category === "arcade") {
+                $("#category-name").text("Old Arcade Games");
+                selectedWord = cars[Math.floor(Math.random() * cars.length)];
+        };
+    
+        console.log("selected word: " + selectedWord);
+        lengthOfWordToGuess = selectedWord.length;
+        wordToGuessArray = selectedWord.split("");
+        for (var i = 0; i < lengthOfWordToGuess; i++) {
+            displayArray.push("_");
+        }
+        $("#guess-area").text(displayArray.join(" "));
+      
+        createButtons();
+    };
+    
+    function createButtons() {
+        $("#letter-area").empty();
+        gameOn = true;
+        for (var i = 0; i < letters.length; i++) {
+            var letterButton = $("<button>");
+            letterButton.addClass("letter letter-btn-color").attr("data-newletter", letters[i]).text(letters[i]);  
+            buttons.append(letterButton);
+          }
+    };
+    
+       
+    buttons.on("click", ".letter", function() {
+        if (gameOn) {
+        selectedLetter = $(this).attr("data-newletter");
+        $(this).hide();
+        
+        doesLetterExist(selectedLetter);
+        didIWin();
+        }
+    });
+     
+    
+    
+    function doesLetterExist(selectedLetter) {
+        isInArray = false;
+        
+        for (var i = 0; i < lengthOfWordToGuess; i++) {
+            if(selectedLetter === wordToGuessArray[i]) {
+                isInArray = true;
+            } 
+        } 
+        
+        if (isInArray) {
+            for (var i = 0; i < lengthOfWordToGuess; i++) {
+                if(selectedLetter === wordToGuessArray[i]) {
+                    displayArray[i] = selectedLetter;
+                }
+                $("#guess-area").text(displayArray.join(" "));  
+            }
+               
+            } else {
+                misses++;
+                console.log("this is misses: " + misses);
+                changeImage(misses);
+                
+            }
+    };
+    
+    function changeImage(misses) {
+        if (misses === 1) {
+            $("#missesPic").attr('src', 'assets/images/2.JPG');
+        } else if (misses === 2) {
+            $("#missesPic").attr('src', 'assets/images/3.JPG');
+        }else if (misses === 3) {
+            $("#missesPic").attr('src', 'assets/images/4.JPG');
+        }else if (misses === 4) {
+            $("#missesPic").attr('src', 'assets/images/5.JPG');
+        }else if (misses === 5) {
+            $("#missesPic").attr('src', 'assets/images/6.JPG');
+        }else if (misses === 6) {
+            $("#missesPic").attr('src', 'assets/images/7.JPG');
+        };
+    };
+    
+    
+    function didIWin() {
+        if (displayArray.toString() === wordToGuessArray.toString()) {
+            wins++;
+            $(".wins").text(wins);
+            $("#missesPic").attr('src', 'assets/images/1.JPG');
+            $(".container-game-screen").hide();
+            $(".container-win-screen").show();
+            winning();
+            
+            // startGame();
+    } else if (misses === 7) {
+            gameOn = false;
+            losses++;
+            $(".losses").text(losses);
+            $(".container-game-screen").hide();
+            $(".container-loss-screen").show();
+    }
+    };
+    
+    $("#nextWord-lost").on("click", function() {
+        startGame(category);
+        $(".container-loss-screen").hide();
+        $(".container-game-screen").show();
+        $(".container-win-screen").hide();
+        $("#missesPic").attr('src', 'assets/images/1.JPG');
+          
+    });
+    
+    $("#nextWord-win").on("click", function() {
+        startGame(category);
+        $(".container-loss-screen").hide();
+        $(".container-game-screen").show();
+        $(".container-win-screen").hide();
+    });
+    
+    
+    $("#redo").on("click", function() {
+        // alert("hi");
+        $(".container-game-screen").hide();
+        $(".container-loss-screen").hide();
+        $(".container-welcome-screen").show();
+        $("#missesPic").attr('src', 'assets/images/1.JPG');
+        wins = 0;
+        losses = 0;
+    });
+    
+    $("#redo-lost").on("click", function() {
+        // alert("hi");
+        $(".container-game-screen").hide();
+        $(".container-loss-screen").hide();
+        $(".container-welcome-screen").show();
+        $("#missesPic").attr('src', 'assets/images/1.JPG');
+        wins = 0;
+        losses = 0;
+    });
+    
+    function winning() {
+        setInterval(nextImage, 300);
+        
+        function nextImage() {
+            
+            $("#winPic").attr('src', 'assets/images/win' + count + '.JPG');
+            count++;
+            if (count === 4) {
+              count = 1;
+            }
+          };
+    };
+    
+    
+    });
+    
+    
+    
+    
